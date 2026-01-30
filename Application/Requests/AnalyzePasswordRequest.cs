aa762b015c79884727fca536a1a6a768aa0fe5f6
Stav Lidor <72824603+StavLidor@users.noreply.github.com>
2026-01-30 11:18:35 +0200

    change dir names to be like visual studio

--- Application/Requests/AnalyzePasswordRequest.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Application.Requests
-{
-    public class AnalyzePasswordRequest
-    {
-        public required string Password { get; set; } = string.Empty;
-    }
-}
--- PasswordAnalysisService.Core.Tests/StrengthRiskEvaluatorTests.cs
+++ PasswordAnalysisService.Core.Tests/StrengthRiskEvaluatorTests.cs
@@ -2,11 +2,6 @@
 using Domain.Enums;
 using Domain.Models;
 using PasswordAnalysisService.Core.Risk;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace PasswordAnalysisService.Core.Tests
 {
